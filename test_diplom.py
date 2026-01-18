c1973011a30f2e2aceeafb9e95a2c3e9bd804c59
IngvaaJerr <ori.jerr@yandex.ru>
2026-01-18 19:35:46 +0300

    Первая версия второй части диплома

--- test_diplom.py
+++ test_diplom.py
@@ -5,3 +5,4 @@ def test_track_search():
     post_response = post_and_get_requests.post_new_order(data.order_body)
     get_response = post_and_get_requests.get_order_track()
     assert get_response.status_code == 200
+# Герр Игорь, 39-я когорта — Финальный проект. Инженер по тестированию плюс
